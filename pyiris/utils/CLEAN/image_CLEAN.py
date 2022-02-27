7e65de48f32bad2941a449db9424dd24787d1f34
Devin-Huyghebaert <devin.huyghebaert@gmail.com>
2022-02-27 12:10:16 +0100

    adding CLEAN directory

---
+++ pyiris/utils/CLEAN/image_CLEAN.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+#CLEAN image by determining points with largest amplitude in image space then subtracting their contributions from visibility values assuming they are point targets
+
+#difficulty will be determining the amplitude to subtract based on imaging algorithm.
