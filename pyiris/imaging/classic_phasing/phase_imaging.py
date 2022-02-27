5d32e900ad13270cc106b16b988a0fdb35f3bb15
Devin-Huyghebaert <devin.huyghebaert@gmail.com>
2022-02-27 12:11:09 +0100

    added CLEAN placeholder

--- pyiris/imaging/classic_phasing/phase_imaging.py
+++ pyiris/imaging/classic_phasing/phase_imaging.py
@@ -1,7 +1,13 @@
+#for irregularly spaced antenna arrays
+
 import numpy as np
 
-#inputs: Frequency, baselines, resolution, FOV, Visibilities, Coherence or raw?, 
+#inputs: Frequency, baselines, resolution, FOV, Visibilities, 
 
 #sum contributions in cartesian space based on visibilities from each baseline
+
 #assume far field (can set distance to 10x far-field equation based on longest baseline)
+
 #synthesize the FOV based on user inputs for angles to calculate
+
+#This should be straightforward
